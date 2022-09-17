a84546a61fddbeca7f4721bf962c96358b326e09
Abhishek Rathore <z.abhirathore@gmail.com>
2022-09-17 22:09:35 +0530

    problem 4 first solution

---
+++ 4.MedianOfTwoSortedArrays/4.MedianOfTwoSortedArrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> combined;
+        int combinedHalfLen = (m+n)/2;
+        int j=0,k=0;
+        for(int i=0; i<=combinedHalfLen; i++){
+            if(k >= nums2.size() || (j < nums1.size() && nums1[j] < nums2[k])){
+                combined.push_back(nums1[j]);
+                j++;
+            } else {
+                combined.push_back(nums2[k]);
+                k++;
+            }
+        }
+        if((m+n)%2 == 0){
+            return (double)(combined[combinedHalfLen] + combined[combinedHalfLen-1]) / 2;
+        }
+        return (double) (combined[combinedHalfLen]);
+    }
+};
